72596b43e12e0f5bf40ec2440c64c6e9dfd3484f
Chris Raiskin <chris.raiskin@standard.com>
2016-11-07 16:23:52 -0800

    TS Classes

--- app.ts
+++ app.ts
@@ -1,20 +1,82 @@
 
-console.log("Default Parameters");
-
-const countdown = (start: number): void => {
-  while (start > 0) {
-    start--;
-  };
-  console.log("Done!", start);
-};
-countdown(10);
+console.log("------ Classes -------");
 
 
-console.log("Rest Parameters");
+class Greeter {
+  greeting: string;
+  
+  constructor(message: string) {
+    this.greeting = message;
+  }
 
-function makeArray(...args: number[]) {
-  return args;
+  greet() {
+    return `Hello, ${this.greeting}`;
+  }
+
 }
-console.log(makeArray(1, 2, 6, 7));
 
+let greeter = new Greeter("world");
+console.log(greeter.greet());
+
+console.log("------ Classes and Inheritance -------");
+
+class Animal {
+  name: string;
+
+  constructor(aName: string) { this.name = aName; }
+  move(distanceInMeters: number = 0) {
+    console.log(`${this.name} moved ${distanceInMeters}m.`);
+  }
+}
+
+class Snake extends Animal {
+  constructor(name: string) { super(name); }
+  move(distanceInMeters = 5) {
+    console.log("Slithering ... ");
+    super.move(distanceInMeters);
+  }
+}
+
+class Horse extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+
+  move(distanceInMeters = 45) {
+    console.log("Galloping ... ");
+    super.move(distanceInMeters);
+  }
+}
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomeno");
+
+sam.move();
+tom.move(34);  
+
+console.log("------ private constuctor and singleton  -------");
+
+class Logger {
+
+  private static _instance: Logger;
+
+  private constructor() {}
+
+  public static getInstance(): Logger {
+    if (!Logger._instance) {
+      Logger._instance = new Logger();
+    }
+    return Logger._instance;
+  }
+
+  public trace(message: string) {
+    console.log('[Trace] ' + message);
+  }
+
+  //  .. more methods
+}
+
+let logger = Logger.getInstance();
+
+logger.trace("Hello World!");
 
