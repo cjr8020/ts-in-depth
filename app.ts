226aaf4cfaa0f248af55ababfac751d10ca16f8c
Chris Raiskin <chris.raiskin@standard.com>
2016-11-09 10:18:05 -0800

    interfaces, function types, indexable types

--- app.ts
+++ app.ts
@@ -1,82 +1,47 @@
 
-console.log("------ Exercise 1 -------");
+console.log("------ Intefaces -------");
 
-class Car {
+console.log("--- optional properties ---");
 
-  public name: string;
-  public acceleration: number = 0;
-
-  public constructor(name: string) {
-    this.name = name;
-  }
-
-  public honk(): void {
-    console.log("Toooooooot!");
-  }
-
-  public accelerate(speed: number): void {
-    this.acceleration = this.acceleration + speed;
-  }
-
+interface SquareConfig {
+  width: number;
+  color?: string;
 }
 
-let car = new Car("BMW");
-car.honk();
-console.log(`car acceleration: ${car.acceleration}`);
-car.accelerate(10);
-console.log(`car acceleration: ${car.acceleration}`);
+function createSquare(config: SquareConfig): {width: number, color: string} {
+  let newSquare = {width: 100, color: "white" };
 
-
-console.log("------ Exercise 2 -------");
-
-class BaseObject {
-  public width: number = 0;
-  public length: number = 0;
-
-  public constructor(width: number, length: number) {
-    this.width = width;
-    this.length = length;
-  }
-}
-
-class Rectangle extends BaseObject {
-
-  public constructor(width: number, length: number) {
-    console.log("creating a Rectangle");
-    super(width, length);
+  if (config.color){
+    newSquare.color = config.color;
   }
 
-  public calcSize(): number {
-    return this.width * this.length;
-  }
-
+  return newSquare;
 }
 
-let rectangle = new Rectangle(5,2);
-console.log(rectangle.calcSize());
+let mySquare = createSquare({width: 100});
+console.log("mySquare: " + mySquare);
 
+console.log("--- indexable type to define unknown property");
 
-console.log("------ Indexable Types and Objects -------");
-
-console.log("---- movies example without object --- ");
-let movies: { [index: string]: string; } = {};
-movies["best-selling"] = "Lord of the rings";
-movies["award-winning"] = "The Hobbit";
-let movieTag: string = "best-selling";
-console.log(`I like ${movieTag} movies like ${movies[movieTag]}`);
-
-
-// Validators to use
-let vals: { [s: string]: string; } = {};
-vals["ZIP code"] = "blah";
-vals["Letters only"] = "bluh";
-console.log(vals["ZIP code"]);
-
-console.log("--- readonly indeces ---");
-interface ReadonlyStringArray {
-  readonly [index: number]: string;
+interface NamedPerson {
+  firstName: string;
+  age?: number;
+  [propName: string]: any;
 }
 
-let myArray: ReadonlyStringArray = ["Alice", "Bob", "Candice"];
-// myArray[3] = "Carrol";   // error
-// myArray[2] = "Mallory";  // error
+function greet(person: NamedPerson): void {
+  console.log(`Hello ${person.firstName}`);
+}
+
+function changeName(person: NamedPerson, newName: string): void {
+  person.firstName = newName;
+}
+
+let max: NamedPerson = {
+  firstName: "Max",
+  hobbies: ["Cooking", "Sports"]
+};
+
+greet(max);
+changeName(max, "Anna");
+greet(max);
