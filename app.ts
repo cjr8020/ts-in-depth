20cd89cee2c606e117da590f28801788aab883b2
Chris Raiskin <chris.raiskin@standard.com>
2016-11-14 11:29:34 -0800

    using jQuery as external JS library with TS

--- app.ts
+++ app.ts
@@ -1,39 +1,9 @@
+import "jQuery";
 
-console.log("------ Decorators -------");
 
-console.log("------ Class Decorators -------");
 
-function observable(constructor: Function){
-  console.log(`Class ${constructor.name} is being created... `);
-}
+console.log("------ using external JS libraries with TS -------");
 
-@observable
-class Greeter {
-  greeting: string;
-  constructor(message: string) {
-    this.greeting = message;
-  }
-  greet() {
-    return "Hello, " + this.greeting;
-  }
-}
+console.log("------ using jQuery -------");
 
-function printable(constructorFn: Function) {
-  constructorFn.prototype.print = function () {
-    console.log(this);
-  }
-}
-
-@printable
-class Plant {
-  name: string = "Green Plant";
-}
-
-const plant: Plant = new Plant();
-plant.print();
-
-console.log("------ Method Decorators -------");
-
-function logging(value: boolean) {
-  return value ? console.log('Hi'): null;
-}
+$("#app").css({"background-color": "green"});
